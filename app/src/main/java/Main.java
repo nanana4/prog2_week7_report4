0c0634e574b342e0fab2334d62b1ff2a73a43165
nanana4 <e245724@ie.u-ryukyu.ac.jp>
2024-12-02 12:44:17 +0900

    step2 commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,7 +2,7 @@ import jp.ac.uryukyu.ie.e245724.*;
 
 public class Main {
     public static void main(String[] args) {
-        Warrior Warrior = new Warrior("勇者", 10, 5);
+        Warrior Warrior = new Warrior("戦士", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", Warrior.getName(), enemy.getName());
